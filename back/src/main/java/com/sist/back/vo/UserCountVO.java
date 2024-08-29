e85961eab97b7e2794e3c16447ddcdd7aefb93f7
chunsickeee <95959518+chunsickeee@users.noreply.github.com>
2024-08-29 15:26:26 +0900

    test22

---
+++ back/src/main/java/com/sist/back/vo/UserCountVO.java
@@ -0,0 +1,14 @@
+package com.sist.back.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCountVO {
+	String cntNotDel, cntDel, cntAll;
+}
