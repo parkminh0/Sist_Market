a92374890b2cfbe264b4fd5e655721e0e9394191
wjsxorjs <wjsxorjs@naver.com>
2024-08-30 16:23:13 +0900

    20240830 api_url modify jeon

--- back/src/main/java/com/sist/back/controller/PostController.java
+++ back/src/main/java/com/sist/back/controller/PostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sist.back.service.PostService;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/adpost")
 public class PostController {
     
     @Autowired
