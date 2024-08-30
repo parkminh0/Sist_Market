5e03d893b4a4816a6f7642056f40eb83b8bc7e54
Asher Morgen <hjoonwoo@gmail.com>
2024-08-30 12:06:36 +0900

    202408301206 heo

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    async rewrites(){
-        return [
-            {
-                source: "/category/:path*",
-                destination: "http://localhost:8080/category/:path*"
-            }
-        ]
-    }
+  async rewrites() {
+    return [
+      {
+        source: "/category/:path*",
+        destination: "http://localhost:8080/category/:path*",
+      },
+      {
+        source: "/post/:path*",
+        destination: "http://localhost:8080/post/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
