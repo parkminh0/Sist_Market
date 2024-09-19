7c7a01e729ed903ee652702c04e48ac790275152
Asher Morgen <hjoonwoo@gmail.com>
2024-09-19 17:35:20 +0900

    202409191735 heo

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,27 +1,26 @@
 /** @type {import('next').NextConfig} */
-        
-const nextConfig = {
 
-    async rewrites(){
-        return [
-            {
-                source: "/category/:path*",
-                destination: "http://localhost:8080/category/:path*"
-            },
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:8080/api/:path*"
-            },
-            {
-              source: "/adpost/:path*",
-              destination: "http://localhost:8080/adpost/:path*",
-            },
-          {
-            source: "/user/:path*",
-            destination: "http://localhost:8080/user/:path*",
-          },
-        ]
-    }
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/category/:path*",
+        destination: "http://13.125.170.142:8080/category/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://13.125.170.142:8080/api/:path*",
+      },
+      {
+        source: "/adpost/:path*",
+        destination: "http://13.125.170.142:8080/adpost/:path*",
+      },
+      {
+        source: "/user/:path*",
+        destination: "http://13.125.170.142:8080/user/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
