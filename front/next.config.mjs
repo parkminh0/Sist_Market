af2c01050ad574038c1833bc2b67aeda4164d547
Asher Morgen <hjoonwoo@gmail.com>
2024-09-23 12:26:00 +0900

    202409231225 heo

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -6,39 +6,39 @@ const nextConfig = {
     return [
       {
         source: "/category/:path*",
-        destination: "http://localhost:8080/category/:path*",
+        destination: "http://13.125.170.142/category/:path*",
       },
       // {
       //     source: "/api/:path*",
-      //     destination: "http://localhost:8080/api/:path*"
+      //     destination: "http://13.125.170.142/api/:path*"
       // },
       {
         source: "/adpost/:path*",
-        destination: "http://localhost:8080/adpost/:path*",
+        destination: "http://13.125.170.142/adpost/:path*",
       },
       {
         source: "/user/:path*",
-        destination: "http://localhost:8080/user/:path*",
+        destination: "http://13.125.170.142/user/:path*",
       },
       {
         source: "/admin/category/:path*",
-        destination: "http://localhost:8080/admin/category/:path*",
+        destination: "http://13.125.170.142/admin/category/:path*",
       },
       {
         source: "/admin/board/:path*",
-        destination: "http://localhost:8080/admin/board/:path*",
+        destination: "http://13.125.170.142/admin/board/:path*",
       },
       {
         source: "/town/:path*",
-        destination: "http://localhost:8080/town/:path*",
+        destination: "http://13.125.170.142/town/:path*",
       },
       {
         source: "/address/:path*",
-        destination: "http://localhost:8080/address/:path*",
+        destination: "http://13.125.170.142/address/:path*",
       },
       {
         source: "/searchlog/:path*",
-        destination: "http://localhost:8080/searchlog/:path*",
+        destination: "http://13.125.170.142/searchlog/:path*",
       },
     ];
   },
