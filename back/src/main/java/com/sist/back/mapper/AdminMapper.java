521fda3fe38ddd927523749b0c66307b0ac21bc5
parkminh0 <pminh98@naver.com>
2024-09-27 18:11:20 +0900

    09251811 park

--- back/src/main/java/com/sist/back/mapper/AdminMapper.java
+++ back/src/main/java/com/sist/back/mapper/AdminMapper.java
@@ -1,10 +1,28 @@
 package com.sist.back.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.sist.back.vo.QnaVO;
+import com.sist.back.vo.admin.PostOverviewVO;
 import com.sist.back.vo.admin.TotalVO;
+import com.sist.back.vo.admin.UserRankVO;
+import com.sist.back.vo.admin.UserStatusCntVO;
 
 @Mapper
 public interface AdminMapper {
     TotalVO getTotal();
+
+    List<PostOverviewVO> postOverview(String year);
+
+    List<String> searchYear();
+
+    List<String> postStatusCnt();
+
+    List<UserStatusCntVO> userStatusCnt();
+
+    List<QnaVO> getQnaList();
+
+    List<UserRankVO> getUserRank();
 }
