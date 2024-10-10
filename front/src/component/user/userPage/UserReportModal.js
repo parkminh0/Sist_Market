0cddce5ff41da0705d442af7ac8adb91c91a7602
wjsxorjs <wjsxorjs@naver.com>
2024-10-11 01:58:58 +0900

    202410110200 UserReportModal jeon

--- front/src/component/user/userPage/UserReportModal.js
+++ front/src/component/user/userPage/UserReportModal.js
@@ -63,7 +63,7 @@ export default function UserReportModal(props) {
                             ) : (
                                 <>
                                     <Typography id="modal-modal-title" variant="subtitle1" component="h2" sx={{ fontWeight: 'bold', mb: 0, fontSize: '16px' }}>
-                                        게시글을 신고하는 이유를 선택해주세요.
+                                        사용자를 신고하는 이유를 선택해주세요.
                                     </Typography>
                                     <List>
                                         {list.length > 0 ? (
