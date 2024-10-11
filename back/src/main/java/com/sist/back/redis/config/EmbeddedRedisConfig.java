227d13e3ca19e00dedbd050abe55922b38a7aab0
Asher Morgen <hjoonwoo@gmail.com>
2024-10-11 09:17:42 +0900

    202410110917 heo

--- back/src/main/java/com/sist/back/redis/config/EmbeddedRedisConfig.java
+++ back/src/main/java/com/sist/back/redis/config/EmbeddedRedisConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sist.back.redis.config;
 
 
@@ -28,34 +27,4 @@ public class EmbeddedRedisConfig {
         // if (redisServer != null) redisServer.stop();
     }
 }
-=======
-package com.sist.back.redis.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import redis.embedded.RedisServer;
-
-
-@Configuration
-public class EmbeddedRedisConfig {
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-    private RedisServer redisServer;
-
-    @PostConstruct
-    public void startRedisServer() {
-        redisServer = new RedisServer(redisPort);
-        redisServer.start();
-    }
-
-    @PreDestroy
-    public void stopRedis() {
-        if (redisServer != null) redisServer.stop();
-    }
-}
->>>>>>> 8ec14e6a8b31b946a7295b51715fe74dac4d03e0
