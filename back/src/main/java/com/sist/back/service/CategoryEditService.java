b49f36617f1770ac9380fe1688342cc2c52c1c2c
Asher Morgen <hjoonwoo@gmail.com>
2024-10-11 15:33:47 +0900

    12

--- back/src/main/java/com/sist/back/service/CategoryEditService.java
+++ back/src/main/java/com/sist/back/service/CategoryEditService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import com.sist.back.mapper.CategoryEditMapper;
@@ -14,14 +15,17 @@ public class CategoryEditService {
     @Autowired
     CategoryEditMapper mapper;
 
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public List<categoryVO> deletedAll(){
         return mapper.deletedAll();
     }
 
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public int addCategory(categoryVO vo){
         return mapper.addCategory(vo);
     }
 
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public int addCategoryDefault(categoryVO vo){
         return mapper.addCategoryDefault(vo);
     }
@@ -30,13 +34,17 @@ public class CategoryEditService {
         return mapper.getCategory(categorykey);
     }
 
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public int editCategory(categoryVO vo){
         return mapper.editCategory(vo);
     }
     
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public int editCategoryDefault(categoryVO vo){
         return mapper.editCategoryDefault(vo);
     }
+    
+    @CacheEvict(value = "categories", allEntries = true)  // 캐시 무효화
     public int deleteCategory(String categorykey){
         return mapper.deleteCategory(categorykey);
     }
